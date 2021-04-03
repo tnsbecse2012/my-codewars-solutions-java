ce8fbc08a39230320b987d361db16d923123f9bb
Sathish.TN <73893756+sathishtn7191@users.noreply.github.com>
2021-04-03 12:38:18 +0530

    Update HighestAndLowest.java

--- src/main/java/codewars/kata/kyu6/HighestAndLowest.java
+++ src/main/java/codewars/kata/kyu6/HighestAndLowest.java
@@ -20,6 +20,7 @@ public class HighestAndLowest {
 }
 
 /**
+Question:
 
 In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
